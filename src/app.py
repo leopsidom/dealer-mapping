951f9112acc35e93bf469636eda92133fd0478fc
Xiaoyun Yang <xiaoyuny@purecars.com>
2017-05-19 11:15:06 -0400

    request proxy added

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+import os, requests
+
 
 from src.common.database import DataBase
 
@@ -7,8 +9,15 @@ app = Flask(__name__)
 app.secret_key = "123"
 
 
+@app.before_first_request
+def set_proxy():
+    os.environ['http_proxy'] = os.environ.get('FIXIE_URL', '')
+    os.environ['https_proxy'] = os.environ.get('FIXIE_URL', '')
+
+
 @app.route('/')
 def home():
+    print("Http proxy: ", os.environ['http_proxy'])
     cur = DataBase().get_connection_default().cursor()
 
     cur.execute("select table_name from information_schema.tables")
